89c6d8f0d81859597bb926c8b9a1a94fb57bd473
cdujardin4000 <51912624+cdujardin4000@users.noreply.github.com>
2021-11-21 13:58:52 +0100

    Mon beau commit :)
    phase 1
    Je n'ai pas su utiliser les helpers bootstrap pour creer la navbar malgré 2 versions testées... Et j'ai un probleme avec l'integration d'images via chemin relatif j'ai donc utilisé des url probleme avec l'onglet des langues il faut clicker sur le lien precedent pour qu'ils fonctionnent

---
+++ templates/Pages/home.php
@@ -0,0 +1,266 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.10.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ */
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
+use Cake\Error\Debugger;
+use Cake\Http\Exception\NotFoundException;
+
+$this->disableAutoLayout();
+
+$checkConnection = function (string $name) {
+    $error = null;
+    $connected = false;
+    try {
+        $connection = ConnectionManager::get($name);
+        $connected = $connection->connect();
+    } catch (Exception $connectionError) {
+        $error = $connectionError->getMessage();
+        if (method_exists($connectionError, 'getAttributes')) {
+            $attributes = $connectionError->getAttributes();
+            if (isset($attributes['message'])) {
+                $error .= '<br />' . $attributes['message'];
+            }
+        }
+    }
+
+    return compact('connected', 'error');
+};
+
+if (!Configure::read('debug')) :
+    throw new NotFoundException(
+        'Please replace templates/Pages/home.php with your own version or re-enable debug mode.'
+    );
+endif;
+
+$cakeDescription = 'CakePHP: the rapid development PHP framework';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'home']) ?>
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+<body>
+    <header>
+        <div class="container text-center">
+            <a href="https://cakephp.org/" target="_blank" rel="noopener">
+                <!--<img alt="MElogo" src="../../resources/img/MElogo-3.png" width="350" />-->
+                <img alt="MElogo" src="https://myemployees.com/wp-content/uploads/2019/03/MElogo-3.png" width="350" />
+            </a>
+            <h1>Vivement Cake chocolat, mais pas la vanilla.</h1>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= $this->Url->build('/') ?>">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page"  href="<?= $this->Url->build('/employees') ?>">Employees</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= $this->Url->build('/departments') ?>">Departments</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"></a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Fr</a></li>
+                                <li><a class="dropdown-item" href="#">En</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="main">
+        <div class="container">
+            <div class="content">
+                <div class="row">
+                    <div class="column">
+                        <div class="message default text-center">
+                            <small>Please be aware that this page will not be shown if you turn off debug mode unless you replace templates/Pages/home.php with your own version.</small>
+                        </div>
+                        <div id="url-rewriting-warning" style="padding: 1rem; background: #fcebea; color: #cc1f1a; border-color: #ef5753;">
+                            <ul>
+                                <li class="bullet problem">
+                                    URL rewriting is not properly configured on your server.<br />
+                                    1) <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/en/installation.html#url-rewriting">Help me configure it</a><br />
+                                    2) <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/en/development/configuration.html#general-configuration">I don't / can't use URL rewriting</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <?php Debugger::checkSecurityKeys(); ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="column">
+                        <h4>Environment</h4>
+                        <ul>
+                        <?php if (version_compare(PHP_VERSION, '7.2.0', '>=')) : ?>
+                            <li class="bullet success">Your version of PHP is 7.2.0 or higher (detected <?= PHP_VERSION ?>).</li>
+                        <?php else : ?>
+                            <li class="bullet problem">Your version of PHP is too low. You need PHP 7.2.0 or higher to use CakePHP (detected <?= PHP_VERSION ?>).</li>
+                        <?php endif; ?>
+
+                        <?php if (extension_loaded('mbstring')) : ?>
+                            <li class="bullet success">Your version of PHP has the mbstring extension loaded.</li>
+                        <?php else : ?>
+                            <li class="bullet problem">Your version of PHP does NOT have the mbstring extension loaded.</li>
+                        <?php endif; ?>
+
+                        <?php if (extension_loaded('openssl')) : ?>
+                            <li class="bullet success">Your version of PHP has the openssl extension loaded.</li>
+                        <?php elseif (extension_loaded('mcrypt')) : ?>
+                            <li class="bullet success">Your version of PHP has the mcrypt extension loaded.</li>
+                        <?php else : ?>
+                            <li class="bullet problem">Your version of PHP does NOT have the openssl or mcrypt extension loaded.</li>
+                        <?php endif; ?>
+
+                        <?php if (extension_loaded('intl')) : ?>
+                            <li class="bullet success">Your version of PHP has the intl extension loaded.</li>
+                        <?php else : ?>
+                            <li class="bullet problem">Your version of PHP does NOT have the intl extension loaded.</li>
+                        <?php endif; ?>
+                        </ul>
+                    </div>
+                    <div class="column">
+                        <h4>Filesystem</h4>
+                        <ul>
+                        <?php if (is_writable(TMP)) : ?>
+                            <li class="bullet success">Your tmp directory is writable.</li>
+                        <?php else : ?>
+                            <li class="bullet problem">Your tmp directory is NOT writable.</li>
+                        <?php endif; ?>
+
+                        <?php if (is_writable(LOGS)) : ?>
+                            <li class="bullet success">Your logs directory is writable.</li>
+                        <?php else : ?>
+                            <li class="bullet problem">Your logs directory is NOT writable.</li>
+                        <?php endif; ?>
+
+                        <?php $settings = Cache::getConfig('_cake_core_'); ?>
+                        <?php if (!empty($settings)) : ?>
+                            <li class="bullet success">The <em><?= $settings['className'] ?>Engine</em> is being used for core caching. To change the config edit config/app.php</li>
+                        <?php else : ?>
+                            <li class="bullet problem">Your cache is NOT working. Please check the settings in config/app.php</li>
+                        <?php endif; ?>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="column">
+                        <h4>Database</h4>
+                        <?php
+                        $result = $checkConnection('default');
+                        ?>
+                        <ul>
+                        <?php if ($result['connected']) : ?>
+                            <li class="bullet success">CakePHP is able to connect to the database.</li>
+                        <?php else : ?>
+                            <li class="bullet problem">CakePHP is NOT able to connect to the database.<br /><?= $result['error'] ?></li>
+                        <?php endif; ?>
+                        </ul>
+                    </div>
+                    <div class="column">
+                        <h4>DebugKit</h4>
+                        <ul>
+                        <?php if (Plugin::isLoaded('DebugKit')) : ?>
+                            <li class="bullet success">DebugKit is loaded.</li>
+                            <?php
+                            $result = $checkConnection('debug_kit');
+                            ?>
+                            <?php if ($result['connected']) : ?>
+                                <li class="bullet success">DebugKit can connect to the database.</li>
+                            <?php else : ?>
+                                <li class="bullet problem">DebugKit is <strong>not</strong> able to connect to the database.<br /><?= $result['error'] ?></li>
+                            <?php endif; ?>
+                        <?php else : ?>
+                            <li class="bullet problem">DebugKit is <strong>not</strong> loaded.</li>
+                        <?php endif; ?>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="column links">
+                        <h3>Getting Started</h3>
+                        <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/en/">CakePHP Documentation</a>
+                        <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/en/tutorials-and-examples/cms/installation.html">The 20 min CMS Tutorial</a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="column links">
+                        <h3>Help and Bug Reports</h3>
+                        <a target="_blank" rel="noopener" href="irc://irc.freenode.net/cakephp">irc.freenode.net #cakephp</a>
+                        <a target="_blank" rel="noopener" href="http://cakesf.herokuapp.com/">Slack</a>
+                        <a target="_blank" rel="noopener" href="https://github.com/cakephp/cakephp/issues">CakePHP Issues</a>
+                        <a target="_blank" rel="noopener" href="http://discourse.cakephp.org/">CakePHP Forum</a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="column links">
+                        <h3>Docs and Downloads</h3>
+                        <a target="_blank" rel="noopener" href="https://api.cakephp.org/">CakePHP API</a>
+                        <a target="_blank" rel="noopener" href="https://bakery.cakephp.org">The Bakery</a>
+                        <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/en/">CakePHP Documentation</a>
+                        <a target="_blank" rel="noopener" href="https://plugins.cakephp.org">CakePHP plugins repo</a>
+                        <a target="_blank" rel="noopener" href="https://github.com/cakephp/">CakePHP Code</a>
+                        <a target="_blank" rel="noopener" href="https://github.com/FriendsOfCake/awesome-cakephp">CakePHP Awesome List</a>
+                        <a target="_blank" rel="noopener" href="https://www.cakephp.org">CakePHP</a>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="column links">
+                        <h3>Training and Certification</h3>
+                        <a target="_blank" rel="noopener" href="https://cakefoundation.org/">Cake Software Foundation</a>
+                        <a target="_blank" rel="noopener" href="https://training.cakephp.org/">CakePHP Training</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ templates/layout/default.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ */
+
+$cakeDescription = 'CakePHP: the rapid development php framework';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+<body>
+    <nav class="top-nav">
+        <div class="top-nav-title">
+            <a href="<?= $this->Url->build('/') ?>"><img src="https://www.cakephp.org/img/flags/We-bake-with-CakePHP.png" width="100px;"></a>
+        </div>
+        <div class="top-nav-links">
+            <a target="_blank" rel="noopener" href="https://moodle.epfc.eu/course/view.php?id=23934">Moodle</a>
+            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+        </div>
+    </nav>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= $this->Url->build('/') ?>">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page"  href="<?= $this->Url->build('/employees') ?>">Employees</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= $this->Url->build('/departments') ?>">Departments</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"></a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Fr</a></li>
+                            <li><a class="dropdown-item" href="#">En</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <main class="main">
+        <div class="container">
+            <?= $this->Flash->render() ?>
+            <?= $this->fetch('content') ?>
+        </div>
+    </main>
+
+    <footer>
+        <p>copyright © EPFC – WFPO 2021 Made with <strong>❤</strong> by Dujardin Cédric  <a href="#">cdujardin4000</a>.</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</body>
+</html>
